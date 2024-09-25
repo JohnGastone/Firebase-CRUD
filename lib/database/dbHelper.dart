dd87ae877cc9e17b6ba60885f9e524e2d82c8887
JohnGastone <johngastone11@gmail.com>
2024-09-25 08:39:51 +0300

    feat: Create and Update Data from DB method

--- lib/database/dbHelper.dart
+++ lib/database/dbHelper.dart
@@ -2,6 +2,7 @@
 
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:sqlitecrud/model/dish.dart';
 
 class Dbhelper {
   late Database _db;
@@ -20,7 +21,7 @@ class Dbhelper {
       CREATE TABLE IF NOT EXISTS Dishes (
         name TEXT NOT NULL,
         description TEXT ,
-        price DOUBLE UNIQUE NOT NULL
+        price DOUBLE  NOT NULL
       );
       ''',
     );
@@ -33,5 +34,19 @@ class Dbhelper {
     } else {
       return _db;
     }
+  }
+
+  //Create Data method
+  Future<int> createDish(Dish dish) async {
+    var dbReady = await db;
+    return await dbReady.rawInsert(
+        "INSERT INTO Dishes(name, description, price) VALUES('${dish.name}', '${dish.description}', '${dish.price}') ");
+  }
+
+  // Update Data Method
+  Future<int> updateDish(Dish dish) async {
+    var dbReady = await db;
+    return await dbReady.rawUpdate(
+        "UPDATE Dishes SET name='${dish.name}', description='${dish.description}', price='${dish.price}' WHERE id=${dish.name}");
   }
 }
