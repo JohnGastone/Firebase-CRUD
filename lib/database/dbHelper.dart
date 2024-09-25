f4c77b9626c956806c94ec8845b331c68c702a13
JohnGastone <johngastone11@gmail.com>
2024-09-25 08:19:54 +0300

    feat: DB existance checker

--- lib/database/dbHelper.dart
+++ lib/database/dbHelper.dart
@@ -1,7 +1,11 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class Dbhelper {
+  late Database _db;
+
   initDb() async {
     String databasePath = await getDatabasesPath();
     String dbPath = join(databasePath, 'CRUDdb.db');
@@ -13,12 +17,21 @@ class Dbhelper {
   void onCreate(Database db, int version) async {
     await db.execute(
       '''
-      CREATE TABLE IF NOT EXISTS users (
-        name INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT NOT NULL,
-        price TEXT UNIQUE NOT NULL
+      CREATE TABLE IF NOT EXISTS Dishes (
+        name TEXT NOT NULL,
+        description TEXT ,
+        price DOUBLE UNIQUE NOT NULL
       );
       ''',
     );
+  }
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initDb();
+      return _db;
+    } else {
+      return _db;
+    }
   }
 }
